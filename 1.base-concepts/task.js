e4d8d870b881bff10c1dd03a9d301731f839a929
TimmyLi <artelaw@gmail.com>
2023-01-17 00:22:46 +0600

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (Math.pow(b)) - (4 * a * c);
+  if (discriminant > 0) {
+    let decision = (-b + Math.sqrt(discriminant) )/(2*a);
+    arr.push(decision);
+    decision = (-b - Math.sqrt(discriminant) )/(2*a)
+    arr.push(decision);
+   } 
+  if (discriminant == 0) {
+    let decision = (-b/(2*a));
+    arr.push(decision);
+  } 
   return arr;
 }
 
