dee90bf3ebafa49a6d417177c972efe50d21b4f0
TimmyLi <artelaw@gmail.com>
2023-01-17 23:34:00 +0600

    Исправление 1-го задания и добавление 2-го задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = (Math.pow(b)) - (4 * a * c);
+  let discriminant = Math.pow(b,2) - 4 * a * c;
   if (discriminant > 0) {
     let decision = (-b + Math.sqrt(discriminant) )/(2*a);
     arr.push(decision);
@@ -16,5 +16,20 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent)){
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
+  } else if (isNaN(countMonths)) {
+    return false;
+}
+let percentForMonth = percent / 100 / 12;
+let bodyCredit = amount - contribution;
+let monthlyPayment = bodyCredit * (percentForMonth + (percentForMonth / ((Math.pow((1 + percentForMonth),countMonths) - 1))));
+let totalAmountToPayment = monthlyPayment * countMonths;
+let totalPaymentRoundingUpTo2 = +totalAmountToPayment.toFixed(2);
+return totalPaymentRoundingUpTo2;
+ 
 }
