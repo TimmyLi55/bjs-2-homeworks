a67f5fe8b4377c5ba8f7c4e11e48c526da221ae6
TimmyLi <artelaw@gmail.com>
2023-02-03 00:41:51 +0600

    Задание 1-3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,86 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[arr.length - 1];
+  let avg = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (arr[i] < min) {
+      min = arr[i]
+    } if (arr[i] > max) {
+      max = arr[i]
+    }
+    
+    sum += arr[i];
+
+
+  }
+
+  avg = sum / arr.length;
+  avg = +avg.toFixed(2) 
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
+let result = arr.reduce((sum, current) => sum + current, 0);
+return result;
 
 }
 
 function differenceMaxMinWorker(...arr) {
+if (arr.length === 0){
+  return 0;
+} 
+let min = Math.min(...arr);
+let max = Math.max(...arr);
+return max-min;
 
 }
 
 function differenceEvenOddWorker(...arr) {
+let sumEvenElement = 0;
+let sumOddElement = 0;
+if (arr.length === 0){
+  return 0;
+} 
+for (element of arr){
+  if ((element % 2) === 0) {
+    sumEvenElement += element;
+  } else {
+    sumOddElement += element;
+  }
+}
+return sumEvenElement - sumOddElement
 
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  if (arr.length === 0){
+    return 0;
+  } 
+  for (element of arr){
+    if ((element % 2) === 0) {
+      sumEvenElement += element;
+      countEvenElement ++; }
+  }
+  return sumEvenElement / countEvenElement;
+}
+function makeWork (arrOfArr, func) {
+let maxWorkerResult = -Infinity;
+let result = -Infinity;
+for (element of arrOfArr){
+  result = func(...element);
+  if (maxWorkerResult < result) {
+    maxWorkerResult = result
+  }
 }
 
-function makeWork (arrOfArr, func) {
+return maxWorkerResult;
+}
+
+function worker (arr) {
 
 }
