73db68b884803eb22c39ab87fbff992bc2fff25b
TimmyLi <artelaw@gmail.com>
2023-02-07 15:05:22 +0600

    Задание 1-2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.isArray === false || arr2.isArray === false) {
+    return console.log("Передаваемое значение должно быть массивом!");
+  } else {
+    return (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]));
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(element => element.gender === gender).reduce((acc, element, index, arr) => {
+    acc += element.age;
+    if (index === arr.length - 1) {
+        return acc / arr.length;
+    }
+    return acc;
+  }, 0);
 }
