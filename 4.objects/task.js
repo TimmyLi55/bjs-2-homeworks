2dce6a89c93dacf258e8d8a410c6671b6358c3de
TimmyLi <artelaw@gmail.com>
2023-02-09 00:04:01 +0600

    Задание 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  this.marks != undefined ? this.marks.push(...marks) : console.log("Студент отчислен");
 }
 
 Student.prototype.getAverage = function () {
-  
+  let result = 0;
+  if (this.marks.length > 0 || this.marks !== undefined) {
+    this.marks.reduce((acc, element, index, arr) => {
+        acc += element;
+        if (index === arr.length - 1) {
+            result = acc / arr.length;
+        }
+        return acc;
+      }, 0);
+  } 
+  return result;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.exclude = reason;
+  delete this.subject;
+  delete this.marks;
 }
